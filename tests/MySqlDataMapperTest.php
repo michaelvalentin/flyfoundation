29308efa197d5f6df6d7a0677b5817323d6aab14
Niklas Markussen <njm@signifly.com>
2014-04-14 14:03:41 +0200

    Working on DataMapper

--- tests/MySqlDataMapperTest.php
+++ tests/MySqlDataMapperTest.php
@@ -7,7 +7,10 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use FlyFoundation\Config;
+use FlyFoundation\Core\Context;
 use FlyFoundation\Database\MySqlDataMapper;
+use FlyFoundation\Factory;
+use FlyFoundation\Models\OpenPersistentEntity;
 use FlyFoundation\SystemDefinitions\EmptyEntityDefinition;
 use FlyFoundation\SystemDefinitions\EntityField;
 use FlyFoundation\SystemDefinitions\EntityFieldType;
@@ -17,7 +20,9 @@ class MySqlDataMapperTest extends \PHPUnit_Framework_TestCase
     /** @var MySqlDataMapper $dataMapper */
     public $dataMapper;
     public $entityDefinition;
-    public $insertData;
+
+    /** @var OpenPersistentEntity[] $insertEntities */
+    public $insertEntities;
     public $insertIds;
     public $updateData;
     public $deleteId;
@@ -30,27 +35,24 @@ class MySqlDataMapperTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-
-        $this->insertData = [
-            ['location_name' => 'Loc 1', 'region_site' => 'Ins'],
-            ['location_name' => 'Loc 2', 'region_site' => 'Ins'],
-            ['location_name' => 'Loc 3', 'region_site' => 'Ins']
-        ];
-
-        $this->insertIds = [1,2,3];
-        $this->updateData = ['id' => 2, 'location_name' => 'Loc 2', 'region_site' => 'Upd'];
-        $this->deleteId = 3;
-
         $this->entityDefinition = new EmptyEntityDefinition();
         $this->entityDefinition->setTableName('local_region_site');
 
         $fieldA = new EntityField('id', EntityFieldType::INTEGER);
+        $fieldA->setPrimaryKey(true);
+
         $fieldB = new EntityField('location_name', EntityFieldType::STRING);
         $fieldC = new EntityField('region_site', EntityFieldType::STRING);
 
         $this->entityDefinition->addField($fieldA);
         $this->entityDefinition->addField($fieldB);
         $this->entityDefinition->addField($fieldC);
+
+        $entityA = new OpenPersistentEntity($this->entityDefinition, ['location_name' => 'Loc 1', 'region_site' => 'Ins']);
+        $entityB = new OpenPersistentEntity($this->entityDefinition, ['location_name' => 'Loc 2', 'region_site' => 'Ins']);
+        $entityC = new OpenPersistentEntity($this->entityDefinition, ['location_name' => 'Loc 3', 'region_site' => 'Ins']);
+
+        $this->insertEntities = [$entityA, $entityB, $entityC];
 
         $config = new Config();
         $config->setMany([
@@ -59,59 +61,50 @@ class MySqlDataMapperTest extends \PHPUnit_Framework_TestCase
              "database_password" => "root",
              "database_name" => "flyfoundation_test"
         ]);
+        $context = new Context();
+        $factory = new Factory($config, $context);
 
         $this->dataMapper = new MySqlDataMapper($this->entityDefinition);
         $this->dataMapper->setConfig($config);
+        $this->dataMapper->setContext($context);
+        $this->dataMapper->setFactory($factory);
         parent::setUp();
     }
 
     public function testSaveInsertSuccess()
     {
         $returnedIds = [];
-        foreach($this->insertData as $data){
-            $returnedIds[] = $this->dataMapper->save($data);
+        foreach($this->insertEntities as $entity){
+            $returnedIds[] = $this->dataMapper->save($entity)['id'];
         }
 
         $this->assertCount(3, $returnedIds);
-        $this->assertEquals($this->insertIds, $returnedIds);
+        $this->assertEquals([1,2,3], $returnedIds);
     }
 
     public function testSaveUpdateSuccess()
     {
-        $returnedId = $this->dataMapper->save($this->updateData);
-        $this->assertEquals($this->updateData['id'], $returnedId);
+        $entity = $this->insertEntities[1];
+        $entity->set('id', 2);
+        $entity->set('region_site', 'Upd');
+        $returnedId = $this->dataMapper->save($entity);
+        $this->assertEquals(['id' => 2], $returnedId);
     }
 
     public function testLoadSuccess()
     {
-        $expected = $this->insertData[0];
-        $expected['id'] = $this->insertIds[0];
-        $result = $this->dataMapper->load($this->insertIds[0]);
-        $this->assertEquals($expected, $result);
+        $entity = $this->insertEntities[0];
+        $entity->set('id', 1);
+        $result = $this->dataMapper->load(1);
+        $this->assertEquals($entity, $result);
     }
 
     public function testDeleteSuccess()
     {
-        $this->dataMapper->delete($this->insertIds[2]);
+        $this->dataMapper->delete(3);
 
         $this->setExpectedException("FlyFoundation\Exceptions\InvalidArgumentException");
-        $this->dataMapper->load($this->insertIds[2]);
-    }
-
-    public function testSaveTooMuchData()
-    {
-        $this->setExpectedException("FlyFoundation\Exceptions\InvalidArgumentException");
-        $data = $this->insertData[0];
-        $data['extra_data'] = 'not allowed';
-        $this->dataMapper->save($data);
-    }
-
-    public function testSaveInvalidId()
-    {
-        $this->setExpectedException("FlyFoundation\Exceptions\InvalidArgumentException");
-        $data = $this->insertData[0];
-        $data['id'] = 4;
-        $this->dataMapper->save($data);
+        $this->dataMapper->load(3);
     }
 
     public function testLoadFailure()
