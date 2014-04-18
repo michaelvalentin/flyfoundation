482e3d9cb6b633fd55a84bf5739d39f10afb48f5
Michael Valentin <mv@signifly.com>
2014-04-18 16:57:47 +0200

    Moved other files to ExampleApp

--- ExampleApp/Controllers/MyModelController.php
+++ ExampleApp/Controllers/MyModelController.php
@@ -1,15 +1,19 @@
 <?php
 
 
-namespace FlyFoundation\Controllers;
+namespace ExampleApp\Controllers;
 
+use FlyFoundation\Controllers\AbstractController;
 use FlyFoundation\Core\Response;
 
 class MyModelController extends AbstractController{
 
     public function view(Response $response, array $arguments)
     {
-        $response->setContent("<p>HER ER ROOT :-)</p>");
+        if(!isset($arguments["alias"])){
+            return false;
+        }
+        $response->setContent("<p>Your argument for the test controller was: ".$arguments["alias"]."</p>");
         return $response;
     }
 
