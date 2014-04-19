3b8d8f8a653218549461a415dcac50a174115619
Michael Valentin <mv@signifly.com>
2014-04-19 12:48:09 +0200

    Added methods in test app

--- tests/TestApp/Controllers/TestAppSpecialController.php
+++ tests/TestApp/Controllers/TestAppSpecialController.php
@@ -5,7 +5,26 @@ namespace TestApp\Controllers;
 
 
 use FlyFoundation\Controllers\AbstractController;
+use FlyFoundation\Core\Response;
 
 class TestAppSpecialController extends AbstractController{
+    public function show(Response $response, array $arguments)
+    {
+        return $response;
+    }
 
+    public function delete(Response $response, array $arguments)
+    {
+        return $response;
+    }
+
+    public function showAll(Response $response, array $arguments)
+    {
+        return $response;
+    }
+
+    public function showFrontPage(Response $response, array $arguments)
+    {
+        return $response;
+    }
 } 
