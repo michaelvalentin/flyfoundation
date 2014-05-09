8fca8a0a6918dc823fa0b1340b3ffefa9e52b8c5
Michael Valentin <mv@signifly.com>
2014-05-09 16:15:50 +0200

    Name Manipulator

---
+++ src/FlyFoundation/Util/nameManipulator.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace FlyFoundation\Util;
+
+
+class NameManipulator {
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function toCamelUpperCaseFirst($name)
+    {
+        return $name;
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function toCamelLowerCaseFirst($name)
+    {
+        return $name;
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function toUnderscored($name)
+    {
+        return $name;
+    }
+} 
